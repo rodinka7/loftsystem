fcbda54f189f98ffa1d2bc292ff7109c042f7aa4
taniadan <taniadan2402@gmail.com>
2020-01-18 00:44:43 +0200

    Mongo config

--- server/config/index.js
+++ server/config/index.js
@@ -1,4 +1,5 @@
-module.exports.MONGO_LOCAL = 'mongodb://localhost:27017/loftsystem';
+// module.exports.MONGO_LOCAL = 'mongodb://localhost:27017/loftsystem';
+module.exports.MONGO_LOCAL = 'mongodb+srv://karmabot:4C3uFAEhoa6W9wkC@cluster0-y78lp.mongodb.net/test?retryWrites=true&w=majority';
 module.exports.SESSION_SECRET = 'bvdzfczbuwp8vvcx';
 module.exports.SESSION_KEY = 'asfcasf67gfg45wpdfghy';
 module.exports.TOKEN_SECRET = 'yZhtj2ayrXXHHKNe';
